31899fefc1ed9f8e49abd26c78920522a7ccd256
Hyeseon Lee <hyesun1999@gmail.com>
2020-03-28 13:40:07 +0900

    fix error

--- simulate_script.js
+++ simulate_script.js
@@ -1,9 +1,9 @@
 (function() {
-    const recoverTime = 8; // 감염 후 완치까지 걸리는 시간(초)
-    const totalCount = 20//200; // 전체 사람 수
+    const recoverTime = 20; // 감염 후 완치까지 걸리는 시간(초)
+    const totalCount = 50;//200; // 전체 사람 수
     const stop_ratio = 0.3; // 멈춰있는 비율
-    const stopCount = 15;//totalCount * stop_ratio; // 멈춘 사람 수
-    const moveCount = 5;//totalCount - stopCount; // 움직이는 사람 수
+    const stopCount = 20;//totalCount * stop_ratio; // 멈춘 사람 수
+    const moveCount = 30;//totalCount - stopCount; // 움직이는 사람 수
     const speed = 1; // 움직이는 속도
     const radius = 5; //반지름
 
@@ -103,7 +103,7 @@
 
         for (let i = 0; i < all_balls.length; i++) {
             ball = all_balls[i];
-            for (let j = i + 1; j < all_balls[j]; j++) {
+            for (let j = i + 1; j < all_balls.length; j++) {
                 testBall=all_balls[j];
                 if (hitTest(ball, testBall)) {
                     if (ball.color === 'red' && testBall.color === 'silver') {
